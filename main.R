1744b96d1f16e65cf594e2c3959887dcae6843a6
mukeshc93 <mukeshc.93@outlook.com>
2016-05-09 21:38:56 +0530

    Update main.R

--- main.R
+++ main.R
@@ -1,16 +1,6 @@
 library(quantmod)
 library(xgboost)
-#data = datacollection()
-#data=data[complete.cases(data),]
-#data1 = data
 
-#data=rd(data)
-#write.csv(data,"data.csv",row.names = F)
-
-#test = datacollection(requiretestdata = T)
-#tdata1 = test
-#testdata=rd(testdata)
-#write.csv(testdata,"testdata.csv",row.names = F)
 
 ml = function(n) {
     xgtrain = xgb.DMatrix(as.matrix(train[, - ncol(train)]), label = train[, ncol(train)], missing = NA)
@@ -35,31 +25,18 @@ ml = function(n) {
     pred = pred / i
     return(pred)
 }
-#pred = rep(0, nrow(test))
-#for (i in 1:n) {
-#set.seed(i+12)
-#pred = pred+predict(bst, xgtest)
-#  }
-pred = ml(2)
+
+pred = as.data.frame(ml(10))
 plot(pred, type = "h")
-error = (test[, ncol(test)] - ((pred)))
-print(mean(abs(error)));
-print(mean(abs(pred)));
-print(mean(abs(test[, ncol(test)])))
-plot(test[, ncol(test)], type = "h") #,ylim=c(-0.5,2))
-points(pred, col = 2)
 
 act = NULL
-pre = NULL
-for (j in 1:nrow(test)) {
-    act[j] = ifelse(test[j, ncol(test)] < 0, 0, 1)
-    pre[j] = ifelse(pred[j] < 0, 0, 1)
+
+for (j in 1:nrow(pred)) {
+    act[j] = ifelse(pred[j, ] < 0, 0, 1)
 }
-table(act, pre)
-random = cbind(act, pre)
-ra = nrow(test) - nrow(random[which(rowSums(random) == 1),]);
-print(ra / nrow(test))
-upstocks = test[as.logical(act), ncol(data)]
+
+upstocks = as.data.frame(cbind(row.names(test[as.logical(act),]),pred[as.logical(act),]))
 write.csv(upstocks, "upstock.csv")
 
-downstocks = test[as.logical(pre), ncol(data)]
+downstocks = as.data.frame(cbind(row.names(test[!as.logical(act),]),pred[!as.logical(act),]))
+write.csv(downstocks, "downstock.csv")
